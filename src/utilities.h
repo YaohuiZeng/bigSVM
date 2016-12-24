00d4546b67965cc55820e53ad1d2cec226601f82
YaohuiZeng <yaohui-zeng@uiowa.edu>
2016-12-23 19:02:01 -0600

    removed template definition in utilities.h

--- src/utilities.h
+++ src/utilities.h
@@ -18,8 +18,4 @@ double sign(double x);
 
 double sum(double *x, int n);
 
-// template<typename T>
-// T sum(T *x, int n);
-template<typename T, typename BMAccessorType>
-double crossprod_bm_Xj_Xk(XPtr<BigMatrix> xMat, IntegerVector& row_idx, int n, int j, int k);
 #endif
